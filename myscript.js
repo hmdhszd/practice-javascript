306ae15e4ed7c1898369e3d38668505627b02052
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-05-17 21:08:52 +0430

    practice while loop in javascript

--- myscript.js
+++ myscript.js
@@ -1,11 +1,10 @@
-var temp = prompt("how's weather? ( enter degree ) ")
+var x = prompt("enter a number : ")
 
-if (temp > 80) {
-    console.log("it's very hot")
-} else if (temp >= 50 && temp <= 80) {
-    console.log("it's hot")
-} else if (temp >= 20 && temp <= 50) {
-    console.log("it's not hot")
-} else {
-    console.log("it's cold")
+while (x > 0) {
+    console.log(x + " is bigger than 0")
+    x -= 1
+    if (x == 1) {
+        console.log("it's ONE")
+        break;
+    }
 }
